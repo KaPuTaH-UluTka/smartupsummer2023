9d7d9079c0b97879da04ed90fff9950bd42d7950
KaPuTaH-UluTka <lexas427@gmail.com>
2023-05-24 18:21:32 +0300

    feat: refresh token logic

--- src/app/vacancies/page.tsx
+++ src/app/vacancies/page.tsx
@@ -2,34 +2,59 @@
 import { superJobApi } from '@/store/api/api';
 import { AuthorizeData } from '@/utils/constants';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux';
-import { setRefreshToken, setToken } from '@/store/reducers/tokenReducer';
+import { setRefreshToken, setToken, setTokenExpiredTime } from '@/store/reducers/tokenReducer';
 import { useEffect } from 'react';
 import Vacancies from '@/page-components/vacancies/vacancies';
 
 export default function VacanciesWrapper() {
-  const { token } = useAppSelector((state) => state.tokenReducer);
+  const { token, refreshToken, ttl } = useAppSelector((state) => state.tokenReducer);
 
   const dispatch = useAppDispatch();
 
-  const [authTrigger, { data: authResponse, isError, isSuccess }] =
+  const [authTrigger, { data: authResponse, isError: isAuthError, isSuccess: isAuthSuccess }] =
     superJobApi.useLazyAuthorizationQuery();
 
+  const [
+    refreshTrigger,
+    { data: refreshResponse, isError: isRefreshError, isSuccess: isRefreshSuccess },
+  ] = superJobApi.useLazyRefreshTokenQuery();
+
   useEffect(() => {
-    if (!token && !isError) {
+    if (!token && !isAuthError) {
       authTrigger(AuthorizeData);
     }
-    if (isSuccess) {
+    if (token && ttl && !isRefreshError) {
+      if (+ttl < Date.now() / 1000 && refreshToken) {
+        refreshTrigger({ user: AuthorizeData, refresh: refreshToken });
+      }
+    }
+    if (isAuthSuccess) {
       dispatch(setToken(authResponse?.access_token));
       dispatch(setRefreshToken(authResponse?.refresh_token));
+      dispatch(setTokenExpiredTime(authResponse?.ttl));
+    }
+    if (isRefreshSuccess) {
+      dispatch(setToken(refreshResponse?.access_token));
+      dispatch(setRefreshToken(refreshResponse?.refresh_token));
+      dispatch(setTokenExpiredTime(refreshResponse?.ttl));
     }
   }, [
     authResponse?.access_token,
     authResponse?.refresh_token,
+    authResponse?.ttl,
     authTrigger,
     dispatch,
-    isError,
-    isSuccess,
+    isAuthError,
+    isAuthSuccess,
+    isRefreshError,
+    isRefreshSuccess,
+    refreshResponse?.access_token,
+    refreshResponse?.refresh_token,
+    refreshResponse?.ttl,
+    refreshToken,
+    refreshTrigger,
     token,
+    ttl,
   ]);
 
   return <Vacancies />;
--- src/store/reducers/tokenReducer.ts
+++ src/store/reducers/tokenReducer.ts
@@ -3,11 +3,13 @@ import { createSlice } from '@reduxjs/toolkit';
 interface TokenState {
   token: string | null;
   refreshToken: string | null;
+  ttl: string | null;
 }
 
 const initialState: TokenState = {
   token: typeof window !== 'undefined' ? localStorage.getItem('token') : null,
   refreshToken: typeof window !== 'undefined' ? localStorage.getItem('refresh') : null,
+  ttl: typeof window !== 'undefined' ? localStorage.getItem('ttl') : null,
 };
 
 export const TokenReducer = createSlice({
@@ -22,8 +24,12 @@ export const TokenReducer = createSlice({
       state.refreshToken = refreshToken.payload;
       localStorage.setItem('refresh', refreshToken.payload);
     },
+    setTokenExpiredTime: (state, ttl) => {
+      state.ttl = ttl.payload;
+      localStorage.setItem('ttl', ttl.payload);
+    },
   },
 });
 
 export default TokenReducer.reducer;
-export const { setToken, setRefreshToken } = TokenReducer.actions;
+export const { setToken, setRefreshToken, setTokenExpiredTime } = TokenReducer.actions;
